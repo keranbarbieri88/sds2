d0e81e783a69a7a5d4c963a12fa75e6414d62b76
Keran Barbieri <keranbarbieri@hotmail.com>
2021-01-05 20:28:25 -0300

    PUT/ orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -41,11 +41,16 @@ public class OrderService {
 		for (ProductDTO p: dto.getProducts()) {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
-		
 		}
 		order = repository.save(order);
 		return new OrderDTO(order);
-		
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		return new OrderDTO(order);
 	}
 
 }
